1c7fc926b375a39023086f5183b043a25225b1aa
Maciej Krüger <mkg20001@gmail.com>
2022-01-28 17:01:57 +0100

    feat: more lint

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -4,20 +4,20 @@
  *
  */
 
-import React, { memo } from 'react';
+import React, { memo } from 'react'
 // import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
-import getTrad from '../../utils/getTrad';
-import { useIntl } from 'react-intl';
+import pluginId from '../../pluginId'
+import getTrad from '../../utils/getTrad'
+import { useIntl } from 'react-intl'
 
-import { Box } from '@strapi/design-system/Box';
-import { Divider } from '@strapi/design-system/Divider';
-import { Select, Option } from '@strapi/design-system/Select';
-import { Typography } from '@strapi/design-system/Typography';
-import { Stack } from '@strapi/design-system/Stack';
+import { Box } from '@strapi/design-system/Box'
+import { Divider } from '@strapi/design-system/Divider'
+import { Select, Option } from '@strapi/design-system/Select'
+import { Typography } from '@strapi/design-system/Typography'
+import { Stack } from '@strapi/design-system/Stack'
 
 const HomePage = () => {
-  const { formatMessage } = useIntl();
+  const { formatMessage } = useIntl()
 
   return (
     <Box paddingTop={6}>
@@ -25,7 +25,7 @@ const HomePage = () => {
         {formatMessage({ id: getTrad('plugin.name'), defaultMessage: 'Seed Import/Export' })}
       </Typography>
     </Box>
-  );
-};
+  )
+}
 
-export default memo(HomePage);
+export default memo(HomePage)
